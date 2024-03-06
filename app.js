9e27fddf635d79c6437f9e1977f152cc683fa75d
balar axit mukeshbhai <axitbalar@gmail.com>
2024-03-06 12:00:22 +0530

    add cluster url

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const deviceRoutes = require('./routes/deviceRoutes');
 
 const app = express();
 const port = 8000; // Change this to whatever port you prefer
-const mongoURI = 'mongodb://localhost:27017/device-api'; // Change this to your MongoDB URI
+const mongoURI = 'mongodb+srv://axitbalar:AXIt%403810@cluster0.a2xzvn6.mongodb.net/'; // Change this to your MongoDB URI
 
 // Connect to MongoDB
 connectToDatabase(mongoURI);
